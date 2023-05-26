db1f151fec611e13a94f8684b1afd998d7144528
Tito Ciuro <tciuro@mac.com>
2023-05-25 18:42:36 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Tito Ciuro on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
